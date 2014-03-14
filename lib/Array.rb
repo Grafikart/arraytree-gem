c92c5988d896c03ffb0bcf5938c9dc91f56b730c
Jonathan Boyer <contact@grafikart.fr>
2014-03-14 17:13:13 +0100

    Added support for array of object with attr_accessor

--- lib/Array.rb
+++ lib/Array.rb
@@ -1,5 +1,3 @@
-require 'pp'
-
 Array.class_eval do
 
     def tree(foreign_key = :parent_id, id_key = :id)
@@ -7,7 +5,11 @@ Array.class_eval do
         grouped.each do |parent_id, children|
           if parent_id && parent_id != 0
             parent = self.select { |e| e[id_key] == parent_id }.first
-            parent[:leaves] = children
+            if parent.respond_to? :leaves
+                parent.leaves = children
+            else
+                parent[:leaves] = children
+            end
           end
         end
         grouped[0]
